4b16254178715c3e4a665e54bff45b1d55a5881a
sankar <sankar.suda@gmail.com>
2016-09-07 17:56:31 +0530

    Introduced BootableProviderInterface and EventListenerProviderInterface

---
+++ src/BootableProviderInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Speedwork package.
+ *
+ * (c) Sankar <sankar.suda@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace Speedwork\Container;
+
+/**
+ * @author Sankar <sankar.suda@gmail.com>
+ */
+interface BootableProviderInterface
+{
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     *
+     * @param Application $app
+     */
+    public function boot(Container $app);
+}
---
+++ src/EventListenerProviderInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Speedwork package.
+ *
+ * (c) Sankar <sankar.suda@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace Speedwork\Container;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Interface for event listener providers.
+ *
+ * @author Sankar <sankar.suda@gmail.com>
+ */
+interface EventListenerProviderInterface
+{
+    public function subscribe(Container $app, EventDispatcherInterface $dispatcher);
+}
